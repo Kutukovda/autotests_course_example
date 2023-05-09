5064fa953f72e31d670859a5b7d357a8966ffc23
Kutukovda <da.kutukov@tensor.ru>
2023-05-09 23:12:19 +0300

    Выполненное ДЗ 3

--- homework 3/task2.py
+++ homework 3/task2.py
@@ -4,7 +4,10 @@
 
 from statistics import mean
 def get_list_info(lst):
-    # Здесь пишем код
+    min_element = min(lst)
+    max_elem = max(lst)
+    sum_list = round(sum(lst), 2)
+    average = round(sum(lst) / 7, 2)
     return min_elem, max_elem, sum_list, average
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
