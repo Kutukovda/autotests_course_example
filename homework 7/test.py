54593f34f766282eebde4f76e7fa113c0f40328e
Kutukovda <da.kutukov@tensor.ru>
2023-06-01 23:47:47 +0300

    Done

--- homework 7/test.py
+++ homework 7/test.py
@@ -1,9 +1,12 @@
-department = 'Разработка', 'УК', 'Нефункциональное тестирование', 'Автотестирование'
-text = "".join(department)
-# count = sorted([text.count(letter) for letter in text], reverse=True)
-# print([6, 6, 6, 6, 5, 5, 3, 3, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-# print(count)
-# # print(set(text))
-
-for i in sorted(set(text), reverse=True):
-    print(i, end=" ")
+# department = 'Разработка', 'УК', 'Нефункциональное тестирование', 'Автотестирование'
+# text = "".join(department)
+# # count = sorted([text.count(letter) for letter in text], reverse=True)
+# # print([6, 6, 6, 6, 5, 5, 3, 3, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+# # print(count)
+# # # print(set(text))
+#
+# for i in sorted(set(text), reverse=True):
+#     print(i, end=" ")
+dot1 = (5, 5)
+dot2 = (3, 4)
+print(dot1 + dot2)
--- homework 8/task_3.py
+++ homework 8/task_3.py
@@ -8,6 +8,11 @@
 
 
 # Здесь пишем код
+def segment(dot1, dot2):
+    try:
+        return sum(dot1 + dot2)
+    except Exception as e:
+        return "".join(list(e.args[0])[::-1])
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 
@@ -27,6 +32,7 @@ test_data = [
 
 
 for i, d in enumerate(data):
+    # print(segment(i, d))
     assert segment(*d) == test_data[i], f'С набором {d} есть ошибка, не проходит проверку'
     print(f'Тестовый набор {d} прошёл проверку')
 print('Всё ок')
